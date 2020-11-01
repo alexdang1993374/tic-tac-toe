9e8ee9af3e68d86ae89a608ee0650fd36a44a3ea
Alex Dang <alexdang1993374@gmail.com>
2020-11-01 14:53:06 +0700

    made board component

---
+++ src/components/Board/Board.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Board.css";
+import { Cell } from "../Cell/Cell";
+
+export const Board = (props) => {
+  const cells = props.cellValues.map((value, index) => {
+    const canHighlight =
+      props.winningCombination && props.winningCombination.indexOf(index) >= 0;
+    return (
+      <Cell
+        key={index}
+        value={value}
+        canHighlight={canHighlight}
+        onClick={() => props.onCellClicked(index)}
+      />
+    );
+  });
+
+  return (
+    <>
+      <div id="board">{cells}</div>
+    </>
+  );
+};
