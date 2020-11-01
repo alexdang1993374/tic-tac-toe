e9990598273f9d660436cb9d1194a702c764881a
Alex Dang <alexdang1993374@gmail.com>
2020-11-01 14:52:51 +0700

    made game component

---
+++ src/components/Game/Game.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./Game.css";
+import { Board } from "../Board/Board";
+import { ResultModal } from "../ResultModal/ResultModal";
+
+export const Game = () => {
+  const [cellValues, setCellValues] = useState([
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ]);
+  const [xIsNext, setXIsNext] = useState(true);
+  const winningCombination = [];
+
+  const isCellEmpty = (cellIndex) => cellValues[cellIndex] === "";
+
+  const onCellClicked = (cellIndex) => {
+    if (isCellEmpty(cellIndex)) {
+      const newCellValues = [...cellValues];
+
+      newCellValues[cellIndex] = xIsNext ? "X" : "O";
+      setCellValues(newCellValues);
+      setXIsNext(!xIsNext);
+    }
+  };
+
+  return (
+    <>
+      <div id="game">
+        <h1>Tic Tac Toe</h1>
+        <Board
+          cellValues={cellValues}
+          winningCombination={winningCombination}
+          onCellClicked={onCellClicked}
+        />
+      </div>
+      <ResultModal />
+    </>
+  );
+};
