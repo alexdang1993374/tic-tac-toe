e97d49090e2117f512b87e719bfb136935bdf1da
Alex Dang <alexdang1993374@gmail.com>
2020-11-01 15:09:14 +0700

    set up calculate winner function

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Game.css";
 import { Board } from "../Board/Board";
 import { ResultModal } from "../ResultModal/ResultModal";
+import { calculateWinner } from "../../utils/WinnerCalculator";
 
 export const Game = () => {
   const [cellValues, setCellValues] = useState([
@@ -24,13 +25,14 @@ export const Game = () => {
   const onCellClicked = (cellIndex) => {
     if (isCellEmpty(cellIndex)) {
       const newCellValues = [...cellValues];
+      newCellValues[cellIndex] = xIsNext ? "X" : "O";
 
       //Calculate the result
+      const calcResult = calculateWinner(newCellValues, cellIndex);
 
-      newCellValues[cellIndex] = xIsNext ? "X" : "O";
       setCellValues(newCellValues);
       setXIsNext(!xIsNext);
-      setIsGameOver(true);
+      setIsGameOver(calcResult.hasResult);
     }
   };
 
